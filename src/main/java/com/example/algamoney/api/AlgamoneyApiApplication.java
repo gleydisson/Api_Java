8f3d8517c6fa6b39328d7992205635dd8412706b
gleydisson <gleydisson.bona@gmail.com>
2019-11-07 09:04:14 -0500

    Ajustado para a versao 11 do java

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -1,5 +1,5 @@
 package com.example.algamoney.api;
- 
+  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
